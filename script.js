d2b412f5d788e80f91238672c8541b9d891962b3
Shivam Singh <shivam.singh_btech24@gsv.ac.in>
2026-04-06 16:33:04 +0530

    Style improvements and Enter key support

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-document.querySelector("#searchbtn").addEventListener('click', function () {
-    let city = document.querySelector("#city_input").value
-    console.log(city);   
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfd1636b5cfcd29be9e2b5a72e67eef4&units=metric`,
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-          console.log(data);
-          if (data.cod == "404") {
-              let error_msg = document.querySelector("#error_msg")
-              error_msg.textContent = "City not found. Please try again.";
-              return
+function get_weather() {
+  let city = document.querySelector("#city_input").value
+  
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfd1636b5cfcd29be9e2b5a72e67eef4&units=metric`,
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        if (data.cod == "404") {
+            let error_msg = document.querySelector("#error_msg")
+            error_msg.textContent = "City not found. Please try again.";
+            return
+        }
+        else {
+            let error_msg = document.querySelector("#error_msg");
+            error_msg.textContent = "";
+            
+            let city_name = document.querySelector("#city_name")
+            city_name.textContent = "City : " + city
+            
+            let temp = document.querySelector("#temperature")
+            temp.textContent = "Temperature : " + data.main.temp + " Degree Celcius"
+            
+            let condition = document.querySelector("#weather_condition")
+            condition.textContent = "Description : " + data.weather[0].description
+            
+            let humidity = document.querySelector("#humidity")
+            humidity.textContent = "Humidity : " + data.main.humidity + "%";
+  
           }
-          else {
-              let error_msg = document.querySelector("#error_msg");
-              error_msg.textContent = "";
-              
-              let city_name = document.querySelector("#city_name")
-              city_name.textContent = "City : " + city
-              
-              let temp = document.querySelector("#temperature")
-              temp.textContent = "Temperature : " + data.main.temp + " Degree Celcius"
-              
-              let condition = document.querySelector("#weather_condition")
-              condition.textContent = "Description : " + data.weather[0].description
-              
-              let humidity = document.querySelector("#humidity")
-              humidity.textContent = "Humidity : " + data.main.humidity + "%";
+    });
+  
+}
 
-            }
-      });
-})
+
+document.querySelector("#searchbtn").addEventListener('click', get_weather) 
+
+    document.querySelector("#city_input").addEventListener('keydown', function (event) {
+      if (event.key === "Enter") {
+        get_weather()
+    }
+  })
